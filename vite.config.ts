e5db360f6e0bcde21199f7ccfd53085fc29e7062
Grishchenko Igor <Sherkiosp@gmail.com>
2025-10-16 16:38:59 +0500

    change config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,12 @@ export default defineConfig({
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'pksep-reqi',
       fileName: format => `pksep-reqi.${format}.js`
+    },
+    rollupOptions: {
+      output: [
+        { format: 'es', entryFileNames: 'pksep-reqi.js' },
+        { format: 'cjs', entryFileNames: 'pksep-reqi.umd.cjs' }
+      ]
     }
   }
 });
